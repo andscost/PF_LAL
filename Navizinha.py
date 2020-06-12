b5fec24c3a93f3f2b98171fb2651fc7fa6a5b899
andscost <andrecosta888@gmail.com>
2020-06-12 16:31:25 -0300

    Telas do jogo atualizadas

--- Navizinha.py
+++ Navizinha.py
@@ -85,7 +85,7 @@ class ball(pygame.sprite.Sprite):
         if self.rect.bottom > HEIGHT or self.rect.top < 0:
             self.speedy *= -1
         # colisao da bola com os players, que randomiza os valores da direção
-        contato = 15 #define a largura da regiao de colisao que fica na frente do retangulo
+        contato = 10 #define a largura da regiao de colisao que fica na frente do retangulo
         #player1
         if self.rect.left < player1.rect.right+contato and self.rect.left > player1.rect.right and self.rect.top < player1.rect.bottom and self.rect.bottom > player1.rect.top:
             pew_sound.play()
@@ -133,101 +133,102 @@ ball = ball(ball_img)
 all_sprites.add(ball)
 
 DONE = 0
-PLAYING = 1
-GAMEOVER = 2
-state = PLAYING
+START = 1
+PLAYING = 2
+GAMEOVER = 3
+state = START
 
 # ===== Loop principal =====
-game = True
-while game:
+while state != DONE:
     clock.tick(FPS)
-    # ----- Trata eventos
-        
-    for event in pygame.event.get():
-        #tela de inicio
+    while state == START:
+        clock.tick(FPS)
+        # ----- Trata eventos
+        for event in pygame.event.get():
+            # ----- Verifica consequências
+            if event.type == pygame.QUIT:
+                state = DONE
+            if event.type == pygame.KEYUP: #recomeçar o jogo
+                state = PLAYING
+                Points[0] = 0
+                Points[1] = 0
         window.fill((0,0,0))  # Preenche com a cor preta
         window.blit(inicio, inicio_rect) 
+        pygame.display.update()
+    pygame.mixer.music.play(loops=-1)  #começa a musica
+    while state == PLAYING: 
+        clock.tick(FPS)
+        # ----- Trata eventos
+        for event in pygame.event.get():
+            # ----- Verifica consequências
+            if event.type == pygame.QUIT:
+                state = DONE
+            # Só verifica o teclado se está no estado de jogo
+            if state == PLAYING:
+                if event.type == pygame.KEYDOWN:
+                    # Dependendo da tecla, altera a velocidade.
+                    #player1
+                    if event.key == pygame.K_w:
+                        player1.speedy = -8
+                    if event.key == pygame.K_s:
+                        player1.speedy = 8
+                    #player2
+                    if event.key == pygame.K_UP:
+                        player2.speedy = -8
+                    if event.key == pygame.K_DOWN:
+                        player2.speedy = 8
+                # Verifica se soltou alguma tecla.
+                if event.type == pygame.KEYUP:
+                    # Dependendo da tecla, altera a velocidade.
+                    #player1
+                    if event.key == pygame.K_w:
+                        player1.speedy = 0
+                    if event.key == pygame.K_s:
+                        player1.speedy = 0
+                    #player2
+                    if event.key == pygame.K_UP:
+                        player2.speedy = 0
+                    if event.key == pygame.K_DOWN:
+                        player2.speedy = 0
+
+
+        # ----- Atualiza estado do jogo
+        # Atualizando a posição dos meteoros
+        all_sprites.update()
+
+        # ----- Gera saídas
+        window.fill((0, 0, 0))  # Preenche com a cor branca
+        window.blit(background, (0, 0))
+        # Desenhando meteoros
+        all_sprites.draw(window)
+
+        # Desenhando o score
+        text_surface = font.render("{}   {}".format(Points[0],Points[1]), True, (255, 255, 0))
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (WIDTH / 2,  10)
+        window.blit(text_surface, text_rect)
+
+        #verifica se a quantidade de pontos para dar game over
+        for pontos in Points:
+            if pontos >= 3:
+                state = GAMEOVER
+                player1.speedy = 0
+                player2.speedy = 0
         pygame.display.update()  # Mostra o novo frame para o jogador
-
-        # ----- Verifica consequências
-        if event.type == pygame.QUIT:
-            game = False
-
-        if event.type == pygame.KEYUP:
-            pygame.mixer.music.play(loops=-1)
-            while state == PLAYING: 
+    pygame.mixer.music.stop()  #para a musica
+    while state == GAMEOVER:
                 clock.tick(FPS)
                 # ----- Trata eventos
                 for event in pygame.event.get():
                     # ----- Verifica consequências
                     if event.type == pygame.QUIT:
                         state = DONE
-                    # Só verifica o teclado se está no estado de jogo
-                    if state == PLAYING:
-                        if event.type == pygame.KEYDOWN:
-                            # Dependendo da tecla, altera a velocidade.
-                            #player1
-                            if event.key == pygame.K_w:
-                                player1.speedy -= 8
-                            if event.key == pygame.K_s:
-                                player1.speedy += 8
-                            #player2
-                            if event.key == pygame.K_UP:
-                                player2.speedy -= 8
-                            if event.key == pygame.K_DOWN:
-                                player2.speedy += 8
-                        # Verifica se soltou alguma tecla.
-                        if event.type == pygame.KEYUP:
-                            # Dependendo da tecla, altera a velocidade.
-                            #player1
-                            if event.key == pygame.K_w:
-                                player1.speedy += 8
-                            if event.key == pygame.K_s:
-                                player1.speedy -= 8
-                            #player2
-                            if event.key == pygame.K_UP:
-                                player2.speedy += 8
-                            if event.key == pygame.K_DOWN:
-                                player2.speedy -= 8
-
-
-                # ----- Atualiza estado do jogo
-                # Atualizando a posição dos meteoros
-                all_sprites.update()
-
-                # ----- Gera saídas
-                window.fill((0, 0, 0))  # Preenche com a cor branca
-                window.blit(background, (0, 0))
-                # Desenhando meteoros
-                all_sprites.draw(window)
-
-                # Desenhando o score
-                text_surface = font.render("{}   {}".format(Points[0],Points[1]), True, (255, 255, 0))
-                text_rect = text_surface.get_rect()
-                text_rect.midtop = (WIDTH / 2,  10)
-                window.blit(text_surface, text_rect)
-
-                #verifica se a quantidade de pontos para dar game over
-                for pontos in Points:
-                    if pontos >= 3:
-                        state = DONE
-
-                pygame.display.update()  # Mostra o novo frame para o jogador
-            state = GAMEOVER
-            while state == GAMEOVER:
-                clock.tick(FPS)
-                # ----- Trata eventos
-                for event in pygame.event.get():
-                    # ----- Verifica consequências
-                    if event.type == pygame.QUIT:
-                        state = DONE
-                    if event.type == pygame.KEYUP: #recomeçar o jogo
+                    if event.type == pygame.KEYDOWN: #recomeçar o jogo
                         state = PLAYING
                         Points[0] = 0
                         Points[1] = 0
                 window.fill((255,255,255))  # Preenche com a cor preta
                 pygame.display.update()  # Mostra o novo frame para o jogador
 # ===== Finalização =====
-
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
 
